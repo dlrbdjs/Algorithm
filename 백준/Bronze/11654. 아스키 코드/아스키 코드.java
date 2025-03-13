6f48fd00f87a0036dbb092677636b19e6325c930
Gyueon Lee <69340907+dlrbdjs@users.noreply.github.com>
2025-03-13 15:34:18 +0900

    [Bronze V] Title: 아스키 코드, Time: 172 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char userInput = sc.next().charAt(0);
+
+        System.out.println((int) userInput);
+
+    }
+}
+
