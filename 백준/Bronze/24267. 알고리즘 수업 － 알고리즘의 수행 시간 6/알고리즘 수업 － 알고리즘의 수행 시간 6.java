c23b8d058e3596bc2f355e03db4039847f302778
Gyueon Lee <69340907+dlrbdjs@users.noreply.github.com>
2025-04-03 18:52:58 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        long n = sc.nextInt();
+        System.out.println(n * (n-1) * (n-2) / 6);
+        System.out.println(3);
+    }
+}
