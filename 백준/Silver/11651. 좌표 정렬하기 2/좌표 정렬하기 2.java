f2ccf530d3217cc2da5042764d9bfb38a5b88bd7
Gyueon Lee <69340907+dlrbdjs@users.noreply.github.com>
2025-05-06 01:53:57 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 656 ms, Memory: 52484 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        // arr을 정렬할 것이다
+        // arr의 각 부분 배열 e1, e2의 값을 비교해 정렬할 것이다
+        // 어떤 조건 if문에 따라 e1, e2의 통째로 옮길 것이다
+        Arrays.sort(arr, (e1, e2) -> {
+            if (e1[1] == e2[1]) {
+                return e1[0] - e2[0];
+            } else {
+                return e1[1] - e2[1];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
